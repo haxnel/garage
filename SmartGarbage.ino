3de450a614e24fb82f2615b51608b116ba1f0ccb
haxnel <amanoyuki442@gmail.com>
2021-03-26 19:49:48 +0700

    Penambahan Library dan code untuk terhubung ke antares

--- SmartGarbage.ino
+++ SmartGarbage.ino
@@ -1,7 +1,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 #include <DHT.h>
- 
+#include <AntaresESP8266HTTP.h>
+
 // Konstruk object LCD dengan alamat I2C
 // Ganti 0x3F sesuai dengan alamat I2C modul kalian
 // Jika tidak tahu dapat menggunakan LCD I2C Scanner
@@ -9,31 +10,50 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 Servo servo;
 
+// sediakan variabel untuk akses ke antares
+#define ACCESSKEY "c0769d7e2f485191:1a356b543f0d7083"
+#define projectName "Smartgarbage"
+#define deviceName "HC-SR04"
+
+// menambahkan SSID dan pass wifi
+#define WIFISSID "Halo"
+#define PASSWORD "@megatron12"
+
 #define trigPin D8
 #define echoPin D7
 #define buzzer D4
 #define dhtpin D5
 #define DHTTYPE DHT11
-
+//membuat sebuah object untuk DHT11
 DHT dht(dhtpin, DHTTYPE);
+//membuat sebuah object untuk antares
+AntaresESP8266HTTP antares(ACCESSKEY);
 
 void setup() {  
-  pinMode(trigPin, OUTPUT ); 
-  pinMode(echoPin, INPUT); 
-  servo.attach(D0); // sinyal yang diterima oleh servo berada pada pin D0
-  pinMode(buzzer, OUTPUT);
-  // Pemanggilan pertama memerlukan parameter jumlah kolom dan baris
-  lcd.begin(16,2);
-  lcd.init();
-  // Nyalakan backlight
-  lcd.backlight();
-  dht.begin(); //Nyalakan Sensor DHT11
-  Serial.begin(115200); //untuk menentukan kecepatan pengiriman dan penerimaan data melalui port serial.
+pinMode(trigPin, OUTPUT ); 
+pinMode(echoPin, INPUT); 
+servo.attach(D0); // sinyal yang diterima oleh servo berada pada pin D0
+pinMode(buzzer, OUTPUT);
+// Pemanggilan pertama memerlukan parameter jumlah kolom dan baris
+lcd.begin(16,2);
+lcd.init();
+Serial.begin(115200); //untuk menentukan kecepatan pengiriman dan penerimaan data melalui port serial.
+// Nyalakan backlight
+lcd.backlight();
+dht.begin(); //Nyalakan Sensor DHT11
+
+WiFi.begin(WIFISSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+     { 
+       delay(1000);
+     }
+  antares.setDebug(true); //Untuk informasi data yang terkirim
+  antares.wifiConnection(WIFISSID, PASSWORD);
 }
  
 void loop(){
  //prosedur untuk mevariabelkan durasi dan jarak
-long durasi, jarak;
+int durasi, jarak;
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2); 
 digitalWrite(trigPin, HIGH);
@@ -48,6 +68,10 @@ jarak = (durasi/2) / 29.1;
 Serial.print("Jarak=");
 Serial.print(jarak);
 Serial.println(" cm");
+
+//membuat vartiabel untuk ditampilkan (dikirimkan) di antares
+antares.add("Jarak: ", jarak);
+
 // Jika jarak kurang dari 25 cm dan lebih sama dengan 0
 if (jarak <= 25 && jarak >= 0) 
 {
@@ -70,6 +94,8 @@ Serial.println(" C");
 Serial.print("Kelembapan= ");
 Serial.print(v);
 Serial.println(" %");
+antares.add("Temperature: ", suhu);
+antares.add("Kelembapan: ", v);
 
   if (v > 75){
     tone(buzzer, 2000);
@@ -90,4 +116,7 @@ Serial.println(" %");
     lcd.print(v);
     lcd.print(" %");
   }
+
+antares.send(projectName, deviceName);
+delay(1000);
 }
